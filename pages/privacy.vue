e752c7e3274b28350a62be13bed6b366ca5a0dce
Neil Van <nvq.alino@gmail.com>
2021-08-29 21:48:03 +0800

    fix formatting of primary pages

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,45 +1,46 @@
 <template>
-	<div>
-		<client-only>
-			<StripeNav />
-		</client-only>
-		<div class="wall">
-			<div class="story-wall lg:mx-auto">
-				<h1 class="text-5xl text-center">Privacy Policy</h1>
-				<div v-for="text in texts">
-					<div class="my-10 px-5 lg:px-32">
-						<h2 class="text-2xl font-bold text-gray-800">{{ text.name }}</h2>
-						<p class="text-gray-700 text-lg">{{ text.description }}</p>
-					</div>
-				</div>
-			</div>
-		</div>
-		<Footer />
+  <div>
+    <client-only>
+      <StripeNav />
+    </client-only>
+    <div class="wall">
+      <div class="story-wall lg:mx-auto">
+        <h1 class="text-5xl text-center">Privacy Policy</h1>
+        <div v-for="(text, index) in texts" :key="index">
+          <div class="my-10 px-5 lg:px-32">
+            <h2 class="text-2xl font-bold text-gray-800">{{ text.name }}</h2>
+            <p class="text-gray-700 text-lg">{{ text.description }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			texts: ''
-		}
-	},
-	created() {
-		this.getTexts();
-	},
-	methods: {
-		getTexts() {
-			this.$axios.$get('https://inkcms.netlify.app/content/serve/privacy.json')
-				.then(response => {
-					this.texts = response.privacy;
-				})
-				.catch(error => {
-					console.log(error);
-				});
-		},
-	},
-}
+  data() {
+    return {
+      texts: "",
+    };
+  },
+  created() {
+    this.getTexts();
+  },
+  methods: {
+    getTexts() {
+      this.$axios
+        .$get("https://inkcms.netlify.app/content/serve/privacy.json")
+        .then((response) => {
+          this.texts = response.privacy;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
@@ -48,6 +49,4 @@ export default {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-
-
 </style>
