0ed80ae30f41ac2916b6f5efcb9663db59111b6c
Neil Van <nvq.alino@gmail.com>
2021-08-30 21:48:27 +0800

    update responsive design of navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="navbar">
-    <div class="flex justify-between max-w-screen-lg lg:mx-auto px-5">
+    <div
+      class="
+        block
+        space-y-4
+        sm:space-y-0
+        sm:flex
+        justify-between
+        max-w-screen-lg
+        lg:mx-auto
+        px-5
+      "
+    >
       <!-- site logo - on left -->
       <div class="flex justify-center sm:justify-start items-center">
         <nuxt-link to="/">
@@ -20,9 +31,7 @@
       <!-- primary links - on right -->
       <div class="flex justify-center sm:justify-end items-center space-x-6">
         <span>
-          <nuxt-link to="/stories" class="nav-link-stripe hover:underline">
-            Stories
-          </nuxt-link>
+          <nuxt-link to="/stories" class="hover:underline"> Stories </nuxt-link>
         </span>
         <button class="btn-white-navbar hover:bg-black hover:text-white">
           <a href="https://app.theexpressiveink.com">Sign Up or Login</a>
@@ -32,34 +41,9 @@
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {};
-  },
-  methods: {
-    closeNav() {
-      this.$store.commit("toggleNav", false);
-    },
-    openNav() {
-      this.$store.commit("toggleNav", true);
-    },
-  },
-  computed: {
-    navStatus() {
-      return this.$store.state.navOverlay;
-    },
-  },
-};
-</script>
-
 <style>
 .navbar {
   @apply relative w-full py-2 text-white bg-black;
-}
-
-.nav-link {
-  @apply text-4xl;
 }
 
 .btn-white-navbar {
