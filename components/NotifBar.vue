69d92198a55c7b1aea366f7d30ae0176f43c695f
Neil Van <nvq.alino@gmail.com>
2021-08-31 11:15:02 +0800

    fix scrolly components formatting

--- components/NotifBar.vue
+++ components/NotifBar.vue
@@ -1,41 +1,52 @@
 <template>
-	<div class="hidden sm:block">
-		<div v-if="notifs">
-			<transition appear name="fade">
-				<div v-for="notif in notifs">
-					<div class="bg-blue-700 text-white text-base flex space-x-5 justify-center px-10 py-2">
-						<p class="font-bold uppercase">{{ notif.name }}</p>
-						<p>{{ notif.description }}</p>
-					</div>
-				</div>
-			</transition>
-		</div>
+  <div class="hidden sm:block">
+    <div v-if="notifs">
+      <transition appear name="fade">
+        <div v-for="(notif, index) in notifs" :key="index">
+          <div
+            class="
+              bg-blue-700
+              text-white text-base
+              flex
+              space-x-5
+              justify-center
+              px-10
+              py-2
+            "
+          >
+            <p class="font-bold uppercase">{{ notif.name }}</p>
+            <p>{{ notif.description }}</p>
+          </div>
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			notifs: ''
-		}
-	},
-	created() {
-		this.getNotifs();
-	},
-	methods: {
-		getNotifs() {
-			this.$axios.$get('https://inkcms.netlify.app/content/serve/generalnotifs.json')
-				.then(response => {
-					//console.log(response.notification);
-					this.notifs = response.notification;
-				})
-				.catch(error => {
-					console.log(error);
-				});
-		},
-	},
-}
+  data() {
+    return {
+      notifs: "",
+    };
+  },
+  created() {
+    this.getNotifs();
+  },
+  methods: {
+    getNotifs() {
+      this.$axios
+        .$get("https://inkcms.netlify.app/content/serve/generalnotifs.json")
+        .then((response) => {
+          //console.log(response.notification);
+          this.notifs = response.notification;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 
 <style>
@@ -44,6 +55,4 @@ export default {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-
-
 </style>
--- components/SideRside.vue
+++ components/SideRside.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="container-rside md:flex">
-  	<div class="graphic-rside">
-  		<div class="graphic-container">
-				<div v-for="pic in picList">
-					<img :src="pic" class="step-pic"/>
-				</div>
-  		</div>
-  	</div>
-  	<div class="content-rside space-y-32">
-			<div v-for="text in content.texts">
-				<div class="step-rside" :data-step="text.pic">{{ text.char }}</div>
-			</div>
-  	</div>
+    <div class="graphic-rside">
+      <div class="graphic-container">
+        <div v-for="(pic, index) in picList" :key="index">
+          <img :src="pic" class="step-pic" />
+        </div>
+      </div>
+    </div>
+    <div class="content-rside space-y-32">
+      <div v-for="(text, index) in content.texts" :key="index">
+        <div class="step-rside" :data-step="text.pic">{{ text.char }}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,191 +20,194 @@ import scrollama from "scrollama";
 import "intersection-observer";
 
 export default {
-	props: ['content'],
-	data() {
-		return {
-			offsetVal: 0.7,
-			debugVal: false,
-			overlayBgDef: '#E7E9EF',
-			stepBgDef: '#E7E9EF',
-			stepTextDef: '#000',
-			stepBg: '',
-			stepRGBA: '',
-			stepText: '',
-			currentPic: '',
-			imageFitDef: 'contain',
-			imageFit: '',
-		}
-	},
-	mounted() {
-		this.loadComponent();
-	},
-	methods: {
-		loadComponent() {
-			const scroller = scrollama();
-			let steps = this.$el.querySelectorAll('.step-rside');
-			let images = this.$el.querySelectorAll('.step-pic');
-			
-			this.stepBg = (this.content.stepbg) ? this.content.stepbg:this.stepBgDef;
-			this.stepText = (this.content.steptext) ? this.content.steptext:this.stepTextDef;
-			this.imageFit = (this.content.imagefit) ? this.content.imagefit:this.imageFitDef;
-			
-			this.hexToRgba(this.stepBg);
-			
-			steps.forEach((step) => {
-				step.style.backgroundColor =  this.stepRGBA;
-				step.style.color =  this.stepText;
-			});
-			
-			images.forEach((image) => {
-				image.style.objectFit =  this.imageFit;
-			});
-			
-			scroller.setup({
-				step: ".step-rside",
-				offset: this.offsetVal,
-				debug: this.debugVal,
-				progress: true,
-			})
-			.onStepProgress(this.handleStepProgress);
-			
-			window.addEventListener("resize", this.handleResize(scroller));
-		},
-		handleStepProgress(response) {
-			// { element, index, progress }
-			let stepImage = response.element.getAttribute('data-step');
-			this.currentPic = stepImage;
-			
-			let activePic = this.$el.querySelector('.now-active');
-			if (activePic) {
-				activePic.classList.remove('now-active');
-			}
-			
-			let picElement = this.$el.querySelector(`[src="${this.currentPic}"]`);
-			picElement.classList.add('now-active');
-			
-			let level = response.progress;
-			response.element.style.opacity = level;
-		},
-		handleResize(scroller) {
-			let step = this.$el.querySelector('.step-rside');
-			let graphic = this.$el.querySelector('.graphic-rside');
-			
-			let overlayBg = (this.content.picbg) ? this.content.picbg:this.overlayBgDef;
-			graphic.style.backgroundColor =  overlayBg;
-      
-      if (this.content.imageheight == 'full' ) {
-      	graphic.style.height =  window.innerHeight + "px";
-      } else {
-      	let graphicHeight = window.innerHeight / 1.5;
-		    let graphicMarginTop = (window.innerHeight - graphicHeight) / 2;
-		    graphic.style.height =  graphicHeight + "px";
-		    graphic.style.top =  graphicMarginTop + "px";
+  props: ["content"],
+  data() {
+    return {
+      offsetVal: 0.7,
+      debugVal: false,
+      overlayBgDef: "#E7E9EF",
+      stepBgDef: "#E7E9EF",
+      stepTextDef: "#000",
+      stepBg: "",
+      stepRGBA: "",
+      stepText: "",
+      currentPic: "",
+      imageFitDef: "contain",
+      imageFit: "",
+    };
+  },
+  mounted() {
+    this.loadComponent();
+  },
+  methods: {
+    loadComponent() {
+      const scroller = scrollama();
+      let steps = this.$el.querySelectorAll(".step-rside");
+      let images = this.$el.querySelectorAll(".step-pic");
+
+      this.stepBg = this.content.stepbg ? this.content.stepbg : this.stepBgDef;
+      this.stepText = this.content.steptext
+        ? this.content.steptext
+        : this.stepTextDef;
+      this.imageFit = this.content.imagefit
+        ? this.content.imagefit
+        : this.imageFitDef;
+
+      this.hexToRgba(this.stepBg);
+
+      steps.forEach((step) => {
+        step.style.backgroundColor = this.stepRGBA;
+        step.style.color = this.stepText;
+      });
+
+      images.forEach((image) => {
+        image.style.objectFit = this.imageFit;
+      });
+
+      scroller
+        .setup({
+          step: ".step-rside",
+          offset: this.offsetVal,
+          debug: this.debugVal,
+          progress: true,
+        })
+        .onStepProgress(this.handleStepProgress);
+
+      window.addEventListener("resize", this.handleResize(scroller));
+    },
+    handleStepProgress(response) {
+      // { element, index, progress }
+      let stepImage = response.element.getAttribute("data-step");
+      this.currentPic = stepImage;
+
+      let activePic = this.$el.querySelector(".now-active");
+      if (activePic) {
+        activePic.classList.remove("now-active");
       }
-      
+
+      let picElement = this.$el.querySelector(`[src="${this.currentPic}"]`);
+      picElement.classList.add("now-active");
+
+      let level = response.progress;
+      response.element.style.opacity = level;
+    },
+    handleResize(scroller) {
+      let step = this.$el.querySelector(".step-rside");
+      let graphic = this.$el.querySelector(".graphic-rside");
+
+      let overlayBg = this.content.picbg
+        ? this.content.picbg
+        : this.overlayBgDef;
+      graphic.style.backgroundColor = overlayBg;
+
+      if (this.content.imageheight == "full") {
+        graphic.style.height = window.innerHeight + "px";
+      } else {
+        let graphicHeight = window.innerHeight / 1.5;
+        let graphicMarginTop = (window.innerHeight - graphicHeight) / 2;
+        graphic.style.height = graphicHeight + "px";
+        graphic.style.top = graphicMarginTop + "px";
+      }
+
       scroller.resize();
-		},
-		hexToRgba(hex) {
-			let color = hex.replace('#', '')
-      let r = parseInt(color.substring(0, 2), 16)
-      let g = parseInt(color.substring(2, 4), 16)
-      let b = parseInt(color.substring(4, 6), 16)
-      let result = 'rgba(' + r + ',' + g + ',' + b + ',' + 0.85 + ')'
+    },
+    hexToRgba(hex) {
+      let color = hex.replace("#", "");
+      let r = parseInt(color.substring(0, 2), 16);
+      let g = parseInt(color.substring(2, 4), 16);
+      let b = parseInt(color.substring(4, 6), 16);
+      let result = "rgba(" + r + "," + g + "," + b + "," + 0.85 + ")";
       this.stepRGBA = result;
-		},
-	},
-	computed: {
-		picList() {
-			let pics = [];
-			
-			this.content.texts.forEach(text => {
-				pics.push(text.pic);
-			});
-			
-			return [...new Set(pics)]
-		},
-	},
-}
+    },
+  },
+  computed: {
+    picList() {
+      let pics = [];
+
+      this.content.texts.forEach((text) => {
+        pics.push(text.pic);
+      });
+
+      return [...new Set(pics)];
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 .container-rside {
-	@apply relative w-full mx-auto py-16;
+  @apply relative w-full mx-auto py-16;
 }
 
 .graphic-rside {
-	@apply sticky top-0 z-0 flex justify-center items-center w-full bg-gray-300 overflow-hidden;
+  @apply sticky top-0 z-0 flex justify-center items-center w-full bg-gray-300 overflow-hidden;
 }
 
 .graphic-container {
-	@apply relative w-full h-full;
+  @apply relative w-full h-full;
 }
 
 .step-pic {
-	@apply absolute object-cover w-full h-full opacity-0;
-	transition: opacity 1s ease-in-out;
+  @apply absolute object-cover w-full h-full opacity-0;
+  transition: opacity 1s ease-in-out;
 }
 
 .step-pic.now-active {
-	opacity: 1;
+  opacity: 1;
 }
 
 .content-rside {
-	@apply relative w-full flex flex-col justify-center items-center;
+  @apply relative w-full flex flex-col justify-center items-center;
 }
 
 .step-rside {
-	@apply flex items-center text-base p-10 opacity-0;
-	min-width: 300px;
-	max-width: 320px;
+  @apply flex items-center text-base p-10 opacity-0;
+  min-width: 300px;
+  max-width: 320px;
 }
 
 .step-rside:last-child {
-	margin-bottom: 8rem;
+  margin-bottom: 8rem;
 }
 
 @media screen and (min-width: 600px) {
-	.step-rside {
-		@apply text-xl;
-		max-width: 420px;
-	}
+  .step-rside {
+    @apply text-xl;
+    max-width: 420px;
+  }
 }
 
 @media screen and (min-width: 768px) {
-	.graphic-rside {
-		@apply w-3/5;
-	}
-	
-	.content-rside {
-		@apply w-2/5;
-	}
-	
-	.step-rside {
-		max-width: 280px;
-	}
-	
-	.step-rside:first-child {
-		margin-top: 10rem;
-	}
-	
-	.step-rside:last-child {
-		margin-bottom: 12rem;
-	}
+  .graphic-rside {
+    @apply w-3/5;
+  }
+
+  .content-rside {
+    @apply w-2/5;
+  }
+
+  .step-rside {
+    max-width: 280px;
+  }
+
+  .step-rside:first-child {
+    margin-top: 10rem;
+  }
+
+  .step-rside:last-child {
+    margin-bottom: 12rem;
+  }
 }
 
 @media screen and (min-width: 1024px) {
-	.step-rside {
-		max-width: 460px;
-	}	
+  .step-rside {
+    max-width: 460px;
+  }
 }
 
 @media screen and (min-width: 1560px) {
-	.step-rside:last-child {
-		margin-bottom: 16rem;
-	}
+  .step-rside:last-child {
+    margin-bottom: 16rem;
+  }
 }
-
-
-
 </style>
