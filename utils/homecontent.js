8a65678b60a4b8e57d388fe9b970ce163ed1c114
Neil Van <nvq.alino@gmail.com>
2021-09-01 21:21:11 +0800

    move homepage scrolly content data to homecontent file

---
+++ utils/homecontent.js
@@ -0,0 +1,125 @@
+const content1 = {
+    sider: {
+        pics: [
+            "/home/innovation_1.svg",
+            "/home/coworking__monochromatic.svg",
+            "/home/hiking_monochromatic.svg",
+            "/home/news_presenter_monochromatic.svg",
+            "/home/brainstorming__monochromatic.svg",
+            "/home/diversity__monochromatic.svg",
+            "/home/world_wide_web_monochromatic.svg",
+            "/home/revenue_.svg",
+            "/home/flying_kite_monochromatic.svg",
+        ],
+        stepbg: "#191919",
+        picbg: "#C4D0DF",
+        steptext: "#fff",
+        imageheight: "center",
+        texts: [
+            { char: "What is scrollytelling?", pic: "/home/innovation_1.svg" },
+            {
+                char: "Basically, new content appears or changes through 'transitions' as readers scroll down or up the web page of your story.",
+                pic: "/home/innovation_1.svg",
+            },
+            {
+                char: "Stories in scrollytelling format engage the readers easier than the usual static article page format.",
+                pic: "/home/coworking__monochromatic.svg",
+            },
+            {
+                char: "Readers can control the pace by scrolling, and you give them a strong reading experience as they feel they are 'exploring' your story.",
+                pic: "/home/hiking_monochromatic.svg",
+            },
+            {
+                char: "Though the technology and concept are not totally new, there are still barriers why scrollytelling isn't prevalent as what we could have expected.",
+                pic: "/home/flying_kite_monochromatic.svg",
+            },
+            {
+                char: "Big names such as ABC News, The New York Times, BBC, and National Geographic have created visually stunning stories thru scrollytelling.",
+                pic: "/home/news_presenter_monochromatic.svg",
+            },
+            {
+                char: "Creating such stories require a lot of resources: writers, researchers, designers, and developers.",
+                pic: "/home/brainstorming__monochromatic.svg",
+            },
+            {
+                char: "Resources that might not be available for a small organization, let alone an individual.",
+                pic: "/home/brainstorming__monochromatic.svg",
+            },
+            {
+                char: "Moreover, platforms that have scrollytelling features are way too expensive for small players.",
+                pic: "/home/revenue_.svg",
+            },
+            {
+                char: "Therefore, The Expressive Ink exists for that individual, for that school club, for that small organization, for that aspiring storyteller.",
+                pic: "/home/diversity__monochromatic.svg",
+            },
+            {
+                char: "Hello! Welcome to the world of scrollytelling. Let's craft your stories for the modern web.",
+                pic: "/home/world_wide_web_monochromatic.svg",
+            },
+        ],
+    },
+};
+
+const content2 = {
+    overlay: {
+        pics: [
+            "/home/search_engine__flatline.svg",
+            "/home/coding__flatline.svg",
+            "/home/user_group_flatline.svg",
+            "/home/authentication_flatline.svg",
+            "/home/report_analysis__flatline.svg",
+            "/home/idea_applications_flatline.svg",
+            "/home/book_lover_flatline.svg",
+        ],
+        stepbg: "#C4D0DF",
+        picbg: "#fff",
+        steptext: "#000",
+        imagefit: "cover",
+        imageheight: "full",
+        texts: [
+            {
+                char: "What is The Expressive Ink?",
+                pic: "/home/search_engine__flatline.svg",
+            },
+            {
+                char: "The Expressive Ink is a platform for crafting stories in the scrollytelling format.",
+                pic: "/home/coding__flatline.svg",
+            },
+            {
+                char: "Our goal is to make scrollytelling easy for content creators, writers, and storytellers.",
+                pic: "/home/user_group_flatline.svg",
+            },
+            {
+                char: "Anyone can sign up and use the platform for FREE. We call platform users as 'editors'.",
+                pic: "/home/authentication_flatline.svg",
+            },
+            {
+                char: "We currently have 3 scrollytelling components:",
+                pic: "/home/report_analysis__flatline.svg",
+            },
+            {
+                char: "(1) the 'Right Side Text' component you've seen above.",
+                pic: "/home/report_analysis__flatline.svg",
+            },
+            {
+                char: "(2) the 'Left Side Text' component which is the opposite of right side??",
+                pic: "/home/report_analysis__flatline.svg",
+            },
+            {
+                char: "And (3) the 'Overlay' component that you are seeing right now.",
+                pic: "/home/report_analysis__flatline.svg",
+            },
+            {
+                char: "We are continually developing more components for you and your readers to experience awesomeness. We welcome your awesome suggestions as well.",
+                pic: "/home/idea_applications_flatline.svg",
+            },
+            {
+                char: "Be inspired by reading stories featured right now!",
+                pic: "/home/book_lover_flatline.svg",
+            },
+        ],
+    },
+};
+
+export { content1, content2 };
